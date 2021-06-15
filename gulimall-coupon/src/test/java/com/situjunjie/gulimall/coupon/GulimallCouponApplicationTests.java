3803465fbd1d1b3a76d84991fa9a4003709de841
tuqi.stjj <situjunjie@come-future.com>
2021-06-15 12:34:52 +0800

    初始化项目结构

---
+++ gulimall-coupon/src/test/java/com/situjunjie/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.situjunjie.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
