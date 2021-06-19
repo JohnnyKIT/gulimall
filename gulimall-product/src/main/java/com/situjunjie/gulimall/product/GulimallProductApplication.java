0a850bcbb1a46ed061a9dc77a3ce369657ba7984
司徒俊杰 <situjunjie@vip.qq.com>
2021-06-20 00:29:17 +0800

    分类表逻辑删除、product服务的gateway网关路由

--- gulimall-product/src/main/java/com/situjunjie/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/situjunjie/gulimall/product/GulimallProductApplication.java
@@ -3,9 +3,11 @@ package com.situjunjie.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.situjunjie.gulimall.product.dao")
+@EnableDiscoveryClient
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
