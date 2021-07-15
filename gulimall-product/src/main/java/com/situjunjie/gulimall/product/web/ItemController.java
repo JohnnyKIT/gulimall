3d577448566a72e94359f220617bf6596af254cc
司徒俊杰 <situjunjie@vip.qq.com>
2021-07-16 00:10:13 +0800

    产品详情页渲染80% 待完成规格属性的渲染

--- gulimall-product/src/main/java/com/situjunjie/gulimall/product/web/ItemController.java
+++ gulimall-product/src/main/java/com/situjunjie/gulimall/product/web/ItemController.java
@@ -5,6 +5,7 @@ import com.situjunjie.gulimall.product.vo.SkuItemVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -16,10 +17,11 @@ public class ItemController {
     SkuInfoService skuInfoService;
 
     @GetMapping("/{skuId}.html")
-    public String dispatchertToSkuPage(@PathVariable("skuId")Long skuId){
+    public String dispatchertToSkuPage(@PathVariable("skuId")Long skuId, Model model){
         log.info("准备查询商品详情数据展示");
         SkuItemVo vo = skuInfoService.querySkuItemVoInfo(skuId);
         log.info("查询到商品详情：{}",vo);
+        model.addAttribute("item",vo);
         return "item";
     }
 }
