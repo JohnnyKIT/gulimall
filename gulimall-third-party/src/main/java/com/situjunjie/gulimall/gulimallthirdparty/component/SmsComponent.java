8379b43203e72a7338d9911490e09c56c8e17a94
司徒俊杰 <situjunjie@vip.qq.com>
2021-07-17 21:36:14 +0800

    短信验证码服务引入

---
+++ gulimall-third-party/src/main/java/com/situjunjie/gulimall/gulimallthirdparty/component/SmsComponent.java
@@ -0,0 +1,45 @@
+package com.situjunjie.gulimall.gulimallthirdparty.component;
+
+import com.situjunjie.gulimall.gulimallthirdparty.util.HttpUtils;
+import lombok.Data;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "third-party.sms")
+@Data
+public class SmsComponent {
+
+    private String host;
+
+    private String path;
+
+    private String method;
+
+    private String appcode;
+
+
+    public void sendSmsCode(String mobile,String code){
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        Map<String, String> querys = new HashMap<String, String>();
+        querys.put("mobile", mobile);
+        querys.put("param", "code:"+code);
+        querys.put("tpl_id", "TP1711063");
+        Map<String, String> bodys = new HashMap<String, String>();
+        try {
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- gulimall-third-party/src/test/java/com/situjunjie/gulimall/gulimallthirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/situjunjie/gulimall/gulimallthirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,13 +1,33 @@
 package com.situjunjie.gulimall.gulimallthirdparty;
 
+import com.situjunjie.gulimall.gulimallthirdparty.component.SmsComponent;
+import com.situjunjie.gulimall.gulimallthirdparty.util.HttpUtils;
+import org.apache.http.HttpResponse;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
-class GulimallThirdPartyApplicationTests {
+@RunWith(SpringRunner.class)
+public class GulimallThirdPartyApplicationTests {
 
+    @Autowired
+    SmsComponent smsComponent;
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+    }
+
+    /**
+     * 测试发送短信验证码
+     */
+    @Test
+    public void SendSms(){
+        smsComponent.sendSmsCode("16657160605","1234");
     }
 
 }
