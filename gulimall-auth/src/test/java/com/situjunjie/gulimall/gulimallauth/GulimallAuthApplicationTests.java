50838c64dfef088afe3eb9656e11a5522d68dfff
司徒俊杰 <situjunjie@vip.qq.com>
2021-07-17 16:30:26 +0800

    搭建Auth认证中心服务

---
+++ gulimall-auth/src/test/java/com/situjunjie/gulimall/gulimallauth/GulimallAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.situjunjie.gulimall.gulimallauth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
