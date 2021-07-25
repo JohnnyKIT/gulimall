3d8c12a42d2dc836003468e5b4aaed60cf12ecf3
司徒俊杰 <situjunjie@vip.qq.com>
2021-07-25 23:49:19 +0800

    引入rabbitMQ

--- gulimall-order/src/main/java/com/situjunjie/gulimall/order/GuliOrderApplication.java
+++ gulimall-order/src/main/java/com/situjunjie/gulimall/order/GuliOrderApplication.java
@@ -1,11 +1,13 @@
 package com.situjunjie.gulimall.order;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.situjunjie.gulimall.order.dao")
+@EnableRabbit
 public class GuliOrderApplication {
 
     public static void main(String[] args) {
--- gulimall-order/src/test/java/com/situjunjie/gulimall/order/GuliOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/situjunjie/gulimall/order/GuliOrderApplicationTests.java
@@ -2,13 +2,22 @@ package com.situjunjie.gulimall.order;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GuliOrderApplicationTests {
+public class GuliOrderApplicationTests {
+
+    @Autowired
+    RabbitAdmin rabbitAdmin;
 
     @Test
     public void contextLoads() {
+        System.out.println("测试"+rabbitAdmin);
     }
 
 }
