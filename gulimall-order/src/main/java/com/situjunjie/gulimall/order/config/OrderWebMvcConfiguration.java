81b1b6910b4f53d58c284ebba5b49ba18b34a183
司徒俊杰 <situjunjie@vip.qq.com>
2021-07-26 22:35:29 +0800

    完成登录状态校验拦截器

---
+++ gulimall-order/src/main/java/com/situjunjie/gulimall/order/config/OrderWebMvcConfiguration.java
@@ -0,0 +1,19 @@
+package com.situjunjie.gulimall.order.config;
+
+import com.situjunjie.gulimall.order.inteceptor.LoginUserInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class OrderWebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    LoginUserInterceptor loginUserInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginUserInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ gulimall-order/src/main/java/com/situjunjie/gulimall/order/inteceptor/LoginUserInterceptor.java
@@ -0,0 +1,38 @@
+package com.situjunjie.gulimall.order.inteceptor;
+
+import com.situjunjie.common.to.MemberEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginUserInterceptor implements HandlerInterceptor {
+
+    /**
+     * 跳转到订单确认页面需要确定登录状态
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+
+
+    ThreadLocal<MemberEntity> threadLocal = new InheritableThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        MemberEntity memberInfo = (MemberEntity) request.getSession().getAttribute("memberInfo");
+        if(memberInfo!=null){
+            threadLocal.set(memberInfo);
+            return true;
+        }else{
+            //没登录则跳回登录页面
+            response.sendRedirect("http://auth.gulimall.com/login.html");
+            return false;
+        }
+    }
+}
