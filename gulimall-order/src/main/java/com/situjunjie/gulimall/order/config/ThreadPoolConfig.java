0269ea23fd8d914217411f373babf8a5a06347b2
司徒俊杰 <situjunjie@vip.qq.com>
2021-07-26 22:05:46 +0800

    整合SpringSession、Redis，页面整合登录状态

---
+++ gulimall-order/src/main/java/com/situjunjie/gulimall/order/config/ThreadPoolConfig.java
@@ -0,0 +1,19 @@
+package com.situjunjie.gulimall.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+
+/**
+ * 配置线程池
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public ExecutorService executorService(ThreadPoolConfigProperties threadPoolConfigProperties){
+        System.out.println("threadPoolConfigProperties="+threadPoolConfigProperties);
+       return new ThreadPoolExecutor(threadPoolConfigProperties.getCorePoolSize(),threadPoolConfigProperties.getMaximumPoolSize(),threadPoolConfigProperties.getKeepAliveTime(), TimeUnit.SECONDS,new LinkedBlockingQueue<>(), Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
+    }
+}
