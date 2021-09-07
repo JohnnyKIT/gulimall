703f326c34589bad7f4db857a5313c3801c7b6f3
司徒俊杰 <situjunjie@vip.qq.com>
2021-09-08 05:30:13 +0800

    引入AspectJ动态代理

--- gulimall-order/src/main/java/com/situjunjie/gulimall/order/GuliOrderApplication.java
+++ gulimall-order/src/main/java/com/situjunjie/gulimall/order/GuliOrderApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 @MapperScan("com.situjunjie.gulimall.order.dao")
 @EnableRabbit
