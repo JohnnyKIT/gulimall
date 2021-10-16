f5a0f6bc00dd24547c3f404aef3b995a870449a3
司徒俊杰 <situjunjie@vip.qq.com>
2021-10-16 21:50:16 +0800

    完成会员订单列表页面渲染

--- gulimall-member/src/main/java/com/situjunjie/gulimall/member/web/MemberWebController.java
+++ gulimall-member/src/main/java/com/situjunjie/gulimall/member/web/MemberWebController.java
@@ -1,10 +1,12 @@
 package com.situjunjie.gulimall.member.web;
 
+import com.alibaba.fastjson.JSON;
 import com.situjunjie.common.to.MemberEntity;
 import com.situjunjie.common.to.MemberOrderReqTo;
 import com.situjunjie.common.utils.R;
 import com.situjunjie.gulimall.member.service.feign.OrderFeignService;
 import com.situjunjie.gulimall.member.interceptor.LoginUserInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * 会员服务相关页面跳转控制器
  */
 @Controller
+@Slf4j
 public class MemberWebController {
 
     @Autowired
@@ -38,6 +41,8 @@ public class MemberWebController {
         R memberOrder = orderFeignService.getMemberOrder(to);
         //加入作用域并返回跳转
         model.addAttribute("memberOrder",memberOrder);
+        //log.info("购物项：{}", JSON.toJSONString(memberOrder));
+
         return "orderList";
     }
 }
