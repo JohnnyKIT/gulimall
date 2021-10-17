1fad8385f47a0acfc47851a2d40e1b3742d1b2ad
司徒俊杰 <situjunjie@vip.qq.com>
2021-10-17 21:24:45 +0800

    秒杀商品关系list接口修改:根据前端传入关联场次id进行查询

--- gulimall-coupon/src/main/java/com/situjunjie/gulimall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ gulimall-coupon/src/main/java/com/situjunjie/gulimall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -11,6 +11,7 @@ import com.situjunjie.common.utils.Query;
 import com.situjunjie.gulimall.coupon.dao.SeckillSkuRelationDao;
 import com.situjunjie.gulimall.coupon.entity.SeckillSkuRelationEntity;
 import com.situjunjie.gulimall.coupon.service.SeckillSkuRelationService;
+import org.springframework.util.StringUtils;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +19,14 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<SeckillSkuRelationEntity> queryWrapper = new QueryWrapper<SeckillSkuRelationEntity>();
+        String promotionSessionId = (String) params.get("promotionSessionId");
+        if(!StringUtils.isEmpty(promotionSessionId)){
+            queryWrapper.eq("promotion_session_id",promotionSessionId);
+        }
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
