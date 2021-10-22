6334a4b9b0f5dd873b5a3e0b92de093eec3a2ea8
司徒俊杰 <situjunjie@vip.qq.com>
2021-10-22 23:22:21 +0800

    定时上传秒杀活动的时候新增分布式锁，只能由一个服务来上传

--- gulimall-seckill/src/main/java/com/situjunjie/gulimallseckill/scheduled/SeckillSkuScheduled.java
+++ gulimall-seckill/src/main/java/com/situjunjie/gulimallseckill/scheduled/SeckillSkuScheduled.java
@@ -1,9 +1,13 @@
 package com.situjunjie.gulimallseckill.scheduled;
 
 import com.situjunjie.gulimallseckill.service.SeckillSkuService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 秒杀商品处理相关的计划任务
@@ -14,9 +18,22 @@ public class SeckillSkuScheduled {
     @Autowired
     SeckillSkuService seckillSkuService;
 
+    @Autowired
+    RedissonClient redissonClient;
+
+    public static final String UPLOAD_SECKILL_LOCK = "UPLOAD_SECKILL_LOCK";
+
     @Scheduled(cron = "0 * * * * ?")//每天凌晨3点定时执行
     public void uploadSeckillSkuLast3Days(){
-        seckillSkuService.uploadSeckillSkuLast3Days();
+        RLock lock = redissonClient.getLock(UPLOAD_SECKILL_LOCK);
+        lock.lock(10l, TimeUnit.SECONDS);
+        try{
+            seckillSkuService.uploadSeckillSkuLast3Days();
+        }finally {
+            lock.unlock();
+        }
+
+
 
     }
 }
